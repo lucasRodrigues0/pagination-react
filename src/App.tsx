0aab8b3f683a6a3ca7152ec86f7fbc1939dddab3
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2025-01-15 14:44:50 -0300

    instalação de dependências

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className="border-2 border-red-500">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -18,7 +17,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)} className='border-red-500'>
           count is {count}
         </button>
         <p>
