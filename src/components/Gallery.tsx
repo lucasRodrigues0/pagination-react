68a177e7392ef677dd7b34598f44f3df434587c2
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2025-01-16 14:18:50 -0300

    layout

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,17 +1,44 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { getAll } from "../service/service"
+import { Card } from "./Card";
 
 export const Gallery = () => {
 
+    const [images, setImages] = useState<Image[]>([]);
+
     useEffect(() => {
-        getAll().then(data => console.log(data?.data));
+        getAll().then(data => {
+            setImages(data);
+        });
     }, [])
 
     return (
-        <section className="flex justify-center w-full">
+        <section className="flex flex-col justify-center w-full">
             <div className="container">
-                <h1 className="text-4xl text-gray-600">Gallery working</h1>
+                <h1 className="text-4xl text-gray-600">Image Gallery</h1>
+            </div>
+            <div className="flex flex-row border-2 border-red-500 wrap">
+                {
+                    images.map((image) => {
+                        return (
+                            <Card key={`image-${image._id}`}>
+                                <div className="">
+                                    <img src={image.url} alt="" className="" />
+                                </div>
+                                <h2>title: {image.title}</h2>
+                                <p>description: {image.description}</p>
+                            </Card>
+                        )
+                    })
+                }
             </div>
         </section>
     )
+}
+
+interface Image {
+    title: string,
+    description: string,
+    url: string,
+    _id: string
 }
--- src/service/service.ts
+++ src/service/service.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getAll = async () => {
     try {
         const response = await axios.get('/api/image/all-no-pag');
-        return response;
+        return response.data;
     } catch (error: any) {
         console.error(error.response.data);
     }
