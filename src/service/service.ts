0c1794da0ac365a8ff0fedc09cda079a9ef8f392
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2025-02-13 16:11:20 -0300

    ajustes de integração com api

--- src/service/service.ts
+++ src/service/service.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.PROD ? import.meta.env.VITE_API_URL : '/api';
+
 export const getAll = async (page: number) => {
     try {
-        const response = await axios.get(`/api/image?page=${page}&resultsPerPage=12`);
+        console.log(`apiurl: ${API_URL}`);
+        const response = await axios.get(`${API_URL}/image?page=${page}&resultsPerPage=12`);
         return response.data;
     } catch (error: any) {
         console.error(error.response.data);
