16c45cd09ae212e191fdbf9ef59df51415251c34
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2025-02-13 16:13:54 -0300

    ajustes

--- src/service/service.ts
+++ src/service/service.ts
@@ -4,7 +4,6 @@ const API_URL = import.meta.env.PROD ? import.meta.env.VITE_API_URL : '/api';
 
 export const getAll = async (page: number) => {
     try {
-        console.log(`apiurl: ${API_URL}`);
         const response = await axios.get(`${API_URL}/image?page=${page}&resultsPerPage=12`);
         return response.data;
     } catch (error: any) {
