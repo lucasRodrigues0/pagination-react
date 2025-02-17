93ff04cd541ae94c80a6b16766a37b99490df1c2
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2025-02-17 16:43:54 -0300

    ajustes visuais

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ interface props {
 
 export const Button = ({ children, onClick, disabled }: PropsWithChildren & props) => {
 
-    const style = disabled ? "h-10 rounded-md text-gray-300 text-2xl" : "h-10 text-gray-600 font-bold text-2xl"
+    const style = disabled ? "opacity-0" : "h-10 text-gray-600 font-bold text-2xl"
 
     return (
         <button className={style} onClick={onClick} disabled={disabled}>
